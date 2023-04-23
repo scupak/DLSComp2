93c15fc425c1af5180919d50279f9bfda55c87f3
scupak <antonibujko@hotmail.com>
2023-04-23 17:36:52 +0200

    Update SearchResult.cs

--- Common/SearchResult.cs
+++ Common/SearchResult.cs
@@ -3,6 +3,7 @@
 public class SearchResult
 {
     public double EllapsedMiliseconds { get; set; }
+    public DateTime SearchDateTime { get; set; }
     public List<String> IgnoredTerms { get; set; } = new List<string>();
     public List<Document> Documents { get; set; } = new List<Document>();
 }
